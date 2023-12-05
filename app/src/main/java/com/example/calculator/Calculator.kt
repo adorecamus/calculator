cf405e411c12f748dd3cf0c2474aaa36da7358ac
Umin <adorecamus@gmail.com>
2023-12-05 21:18:31 +0900

    Refactor: change property to private

--- app/src/main/java/com/example/calculator/Calculator.kt
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -1,9 +1,6 @@
 package com.example.calculator
 
-class Calculator(abstractOperation: AbstractOperation) {
-
-    val operation = abstractOperation
-
+class Calculator(private val operation: AbstractOperation) {
     fun operate(num1: Int, num2: Int): Double {
         return operation.operate(num1, num2)
     }
