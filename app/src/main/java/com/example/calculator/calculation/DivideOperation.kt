8ba6823a7ce0ddcfb2dc301b70e2469d51a3919a
Umin <adorecamus@gmail.com>
2023-12-08 15:46:55 +0900

    Prevent dividing by zero

--- app/src/main/java/com/example/calculator/calculation/DivideOperation.kt
+++ app/src/main/java/com/example/calculator/calculation/DivideOperation.kt
@@ -3,5 +3,10 @@ package com.example.calculator.calculation
 import com.example.calculator.abstract.AbstractOperation
 
 class DivideOperation : AbstractOperation() {
-    override fun operate(num1: Int, num2: Int): Double = num1 / num2.toDouble()
+    override fun operate(num1: Int, num2: Int): Double {
+        require(num2 != 0) {
+            ArithmeticException("0으로 나눌 수 없습니다")
+        }
+        return num1 / num2.toDouble()
+    }
 }
