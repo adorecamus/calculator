498706320618f52a312149493a85b4574a726360
Umin <adorecamus@gmail.com>
2023-12-08 15:10:31 +0900

    Refactor: change calculator instances to create only once

--- app/src/main/java/com/example/calculator/Main.kt
+++ app/src/main/java/com/example/calculator/Main.kt
@@ -1,6 +1,19 @@
 package com.example.calculator
 
+import com.example.calculator.calculation.AddOperation
+import com.example.calculator.calculation.Calculator
+import com.example.calculator.calculation.DivideOperation
+import com.example.calculator.calculation.MultiplyOperation
+import com.example.calculator.calculation.RemainderOperation
+import com.example.calculator.calculation.SubtractOperation
+
 fun main() {
+    val addCalculator = Calculator(AddOperation())
+    val subtractCalculator = Calculator(SubtractOperation())
+    val multiplyCalculator = Calculator(MultiplyOperation())
+    val divideCalculator = Calculator(DivideOperation())
+    val remainderCalculator = Calculator(RemainderOperation())
+
     while (true) {
         println("숫자를 입력하세요.")
         val num1 = readln().toInt()
@@ -10,12 +23,29 @@ fun main() {
         val num2 = readln().toInt()
 
         when (operator) {
-            "+" -> println("${num1} 더하기 ${num2} 결과는 ${Calculator(AddOperation()).operate(num1, num2)}입니다.")
-            "-" -> println("${num1} 빼기 ${num2} 결과는 ${Calculator(SubstractOperation()).operate(num1, num2)}입니다.")
-            "*" -> println("${num1} 곱하기 ${num2} 결과는 ${Calculator(MultiplyOperation()).operate(num1, num2)}입니다.")
-            "/" -> println("${num1} 나누기 ${num2} 결과는 ${Calculator(DivideOperation()).operate(num1, num2)}입니다.")
-            "%" -> println("${num1} 나누기 ${num2}의 나머지는 ${Calculator(RemainderOperation()).operate(num1, num2)}입니다.")
-            else -> println("지원하지 않는 연산입니다.")
+            "+" -> {
+                println("${num1} 더하기 ${num2} 결과는 ${addCalculator.operate(num1, num2)}입니다.")
+            }
+
+            "-" -> {
+                println("${num1} 빼기 ${num2} 결과는 ${subtractCalculator.operate(num1, num2)}입니다.")
+            }
+
+            "*" -> {
+                println("${num1} 곱하기 ${num2} 결과는 ${multiplyCalculator.operate(num1, num2)}입니다.")
+            }
+
+            "/" -> {
+                println("${num1} 나누기 ${num2} 결과는 ${divideCalculator.operate(num1, num2)}입니다.")
+            }
+
+            "%" -> {
+                println("${num1} 나누기 ${num2}의 나머지는 ${remainderCalculator.operate(num1, num2)}입니다.")
+            }
+
+            else -> {
+                println("그럴 리가")
+            }
         }
 
         println("계속하려면 y를 입력하세요.")
